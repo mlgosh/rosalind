949db7e63c9ff2081f5bb430caba3c8453f7b420
maria <maria.lock@gosh.nhs.uk>
2018-12-19 10:41:16 +0000

    Added original Rosalind solutions

---
+++ SUBS.py
@@ -0,0 +1,14 @@
+def motif_finder(dna, motif):
+    motifs = []
+    initial_length = len(dna)
+    find = dna.find(motif)
+    dna = dna[(find + 1):]
+    motifs.append(find + 1)
+    while dna.find(motif) >= 0:
+        find = dna.find(motif)
+        new_length = len(dna)
+        dna = dna[(find + 1):]
+        motifs.append(find + 1 + (initial_length - new_length))
+    return motifs
+
+print(*motif_finder("GATATATGCATATACTT", "ATAT"), sep = " ")
