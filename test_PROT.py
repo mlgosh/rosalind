02293eb33150980041c3f4ac910829b3fc23042b
Stefan Piatek <stefan.piatek@gosh.nhs.uk>
2018-12-19 15:10:47 +0000

    Add tests for PROT

---
+++ test_PROT.py
@@ -0,0 +1,23 @@
+import pytest
+
+from . import PROT
+
+def test_basic_case():
+    rna_in = "AUGUUCAAG"
+    protein_out = "MFK"
+    assert protein_out == PROT.rna_to_pro(rna_in)
+
+def test_mixed_case():
+    rna_in = "auGUUCAAG"
+    protein_out = "MFK"
+    assert protein_out == PROT.rna_to_pro(rna_in)
+
+def test_two_stops():
+    rna_in = "AUGUAGUUCAAGUAA"
+    protein_out = "M"
+    assert protein_out == PROT.rna_to_pro(rna_in)
+
+def test_non_triplet_sequence():
+    rna_in = "AUGUUCAA" # 8 bases in input
+    with pytest.raises(AssertionError):
+        PROT.rna_to_pro(rna_in)
