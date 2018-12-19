a14f0bba7b42b2d83e29487e5f125099a24664b8
Maria <43235308+mlgosh@users.noreply.github.com>
2018-12-19 11:08:20 +0000

    Added comment to PROT.py

--- PROT.py
+++ PROT.py
@@ -1,3 +1,4 @@
+#Dictionary containing codons with their associated amino acid 
 codons = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
           "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
           "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
